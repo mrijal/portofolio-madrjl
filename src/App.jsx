09af12c3c00c8a4ac7c6c128e86ceb62c232d990
mrijal <mrijal110@gmail.com>
2024-02-08 23:23:10 +0700

    Add:Pembuatan bagian header dan home/hero section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header.jsx'
+import Home from './components/home/Home.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+
+    <main className='main'>
+      <Home/>
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://instagram.com/madrjl" className="home__social-icon" target='_blank'>
+            <i className='uil uil-instagram'></i>
+        </a>
+        
+        <a href="https://github.com/mrijal" className="home__social-icon" target='_blank'>
+            <i className='uil uil-github-alt'></i>
+        </a>
+        
+        <a href="https://linkedin.com/mdrjl07" className="home__social-icon" target='_blank'>
+            <i className='uil uil-linkedin'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
